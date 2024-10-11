fc8ae853f144112a1a403f40b1712dcbd3acf4ff
Burak Keskin <me@burak.dev>
2024-10-11 15:51:09 +0300

    values-info feature added

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setup(
     ],
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
+        "License :: OSI Approved :: GNU License",
         "Operating System :: OS Independent",
     ],
     python_requires=">=3.6",
